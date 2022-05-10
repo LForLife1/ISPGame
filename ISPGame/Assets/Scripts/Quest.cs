2c8b0e8e2b0fd139bec556024f0e251becfad5da
LForLife1 <104854601+LForLife1@users.noreply.github.com>
2022-05-10 00:05:12 -0400

    fixed aspect ratio problems

---
+++ ISPGame/Assets/Scripts/Quest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+
+    public bool isActive;
+
+    public string title;
+    public string description;
+    public string dialogReward;
+}
